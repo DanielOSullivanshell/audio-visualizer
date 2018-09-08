448f0f2cd991524c42ae14740cf3f6117f7e15b3
saberslay <dansteamshell@gmail.com>
2018-09-09 00:27:00 +0100

    get the data of the audio spectrum

---
+++ Assets/Scripts/AudioPeer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+public class AudioPeer : MonoBehaviour {
+    AudioSource _audioSource;
+    public static float[] _samples = new float[512];
+	
+	void Start () {
+        _audioSource = GetComponent<AudioSource>();
+	}
+	
+	void Update () {
+        GetspectrumAudioSource();
+	}
+
+    void GetspectrumAudioSource() {
+        _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+    }
+}
